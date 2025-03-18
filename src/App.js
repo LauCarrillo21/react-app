b7bffbc7ef8cf9de99394bc01c4428914c6c1502
LauCarrillo21 <laucarrillo21@gmail.com>
2025-03-18 12:10:30 -0600

    add css styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import Weather from "./Weather";
 
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> React Weather App </h1>
-        <button className="btn btn-primary">Test Button</button>
+        <Weather />
         <footer>
         This project was created by
           <a href="https://lau-carrillo.netlify.app/"> Lau Carrillo</a>
---
+++ src/Weather.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+    return (
+    <div className="Weather">
+     <form>
+      <div className="row">
+       <div className="col-9">
+        <input type="search"
+               placeholder="Enter a city..."
+               className="form control"
+               autoFocus="on"/>
+       </div>
+       <div className="col-3">
+        <input type="submit" value="Search"
+               className="btn btn-primary w-100"/>
+       </div>
+      </div>
+      </form>
+
+      <h1>Cuba</h1>
+      <ul>
+        <li>Friday 11:00</li>
+        <li>Sunny</li>
+      </ul>
+
+      <div className="row">
+       <div className="col-6">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="sunny" className="folat-left"/>
+       
+            <span className="temperature">24</span>
+            <span className="unit">°C</span>
+       </div>
+       <div className="col-6">
+       <ul>
+        <li>Precipitation: 15%</li>
+        <li>Humidity: 72%</li>
+        <li>Wind: 13km/h</li>
+       </ul>
+       </div>
+      </div>
+    </div>
+    )
+}
