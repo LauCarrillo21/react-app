e5c831376e5387c92305ec7f391b833c33bb8918
LauCarrillo21 <laucarrillo21@gmail.com>
2025-03-17 18:30:55 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 export default function App() {
   return (
     <div className="App">
-      <h1> React Weather App </h1>
-      <footer>
-      This project was created by
-        <a href="https://lau-carrillo.netlify.app/"> Lau Carrillo</a>
-      and is 
-        <a href="https://github.com/LauCarrillo21/react-app"> open-sourced on Github </a>
-      </footer>
+      <div className="container">
+        <h1> React Weather App </h1>
+        <button className="btn btn-primary">Test Button</button>
+        <footer>
+        This project was created by
+          <a href="https://lau-carrillo.netlify.app/"> Lau Carrillo</a>
+        and is 
+          <a href="https://github.com/LauCarrillo21/react-app"> open-sourced on Github </a>
+        </footer>
+      </div>
     </div>
   );
 }
